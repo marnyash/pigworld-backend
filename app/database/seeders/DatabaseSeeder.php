89780a87198f267880e33d510a50b6d1a823f6ff
marnyash <marnyash@gmail.com>
2026-08-25 02:34:06 -0400

    Refactor DatabaseSeeder to use updateOrCreate for user and firstOrCreate for farm

--- app/database/seeders/DatabaseSeeder.php
+++ app/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Farm;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,11 +16,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
+        $user = User::updateOrCreate(['email' => 'test@example.com'], [
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'password' => 'password',
+            'role' => 'farmOwner',
         ]);
+
+        $farm = Farm::firstOrCreate([
+            'name' => 'Green Valley Farm',
+        ], [
+            'location' => 'Kenya',
+        ]);
+
+        $user->farms()->syncWithoutDetaching([$farm->id]);
     }
 }
